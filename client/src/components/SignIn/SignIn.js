6388eca5cee93b7fe77aa75809c45392d4de4492
LoneHippie <lonehippie.music@gmail.com>
2022-01-09 05:02:17 +0200

    progress on styling and functionality for SignIn

--- client/src/components/SignIn/SignIn.js
+++ client/src/components/SignIn/SignIn.js
@@ -8,6 +8,7 @@ const SignIn = () => {
     const socket = useContext(SocketContext);
 
     const [ username, setUsername ] = useState('');
+    const [ room, setRoom ] = useState('general');
 
     useEffect(() => {
         socket.on('message', data => {
@@ -19,16 +20,54 @@ const SignIn = () => {
         return () => socket.close();
     }, [])
 
+    const handlers = {
+        handleChangeName: (e) => {
+            setUsername(e.target.value);
+        },
+        handleChangeRoom: (e) => {
+            setRoom(e.target.value);
+            console.log(room)
+        }
+    }
+
     return (
         <div className={styles.body}>
             <h1 className={styles.title}>Quick Chat</h1>
-            <input 
-                type="text"
-                title='username'
-                placeholder='Your username'
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
+
+            <div className={styles.form_group}>
+                <label htmlFor="username">Username</label>
+                <input 
+                    id="username"
+                    className={styles.input}
+                    type="text"
+                    title='username'
+                    placeholder='Your username'
+                    value={username}
+                    onChange={(e) => handlers.handleChangeName(e)}
+                />
+            </div>
+
+            <div className={styles.form_group}>
+                <label htmlFor='room'>Room</label>
+                <select 
+                    name="rooms" 
+                    id="rooms" 
+                    className={styles.select}
+                    onChange={(e) => handlers.handleChangeRoom(e)}
+                >
+                    <option value="general">General Chat</option>
+                    <option value="javascript">JavaScript</option>
+                    <option value="backend">Back End</option>
+                    <option value="hobbies">Hobbies</option>
+                </select>
+            </div>
+
+            <button
+                className={styles.button}
+                onClick={() => console.log({ username, room })}
+            >
+                Join
+            </button>
         </div>
     )
 };
