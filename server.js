71e5021fcbe4afe44cd25a2e32462c9de4bf4930
LoneHippie <lonehippie.music@gmail.com>
2022-01-25 02:32:23 +0200

    made process.env port default

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ io.on('connection', (socket) => {
     })
 })
 
-const PORT = 3000 || process.env.port;
+const PORT = process.env.port || 3000;
 
 server.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
